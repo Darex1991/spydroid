759393b53a0378f08c16348e8b40feb7bda3e487
dpienczykowski <dpienczykowski@gmail.com>
2017-03-27 21:51:38 +0200

    try to fix ios and android

---
+++ ios/spydroid-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  spydroid-Bridging-Header.h
+//  spydroid
+//
+//  Created by Dariusz Pieńczykowski on 27/03/2017.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#ifndef spydroid_Bridging_Header_h
+#define spydroid_Bridging_Header_h
+
+
+#endif /* spydroid_Bridging_Header_h */
